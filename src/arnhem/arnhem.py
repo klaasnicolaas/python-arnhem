e9b58101d930a7750a6ca5daa4d8f51360ecf727
Klaas Schoute <klaas_schoute@hotmail.com>
2026-09-07 00:43:12 +0200

    Modernize package maintenance and security baseline

--- src/arnhem/arnhem.py
+++ src/arnhem/arnhem.py
@@ -19,7 +19,7 @@ from .exceptions import (
 )
 from .models import ParkingSpot
 
-VERSION: str = metadata.version(__package__)  # ty:ignore[invalid-argument-type]
+VERSION: str = metadata.version("arnhem")
 
 
 @dataclass
